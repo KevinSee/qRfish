6282d8884ce690f12cfddb0ee89a7a1d9c600bc1
Kevin See <Kevin@qcinc.org>
2016-09-30 13:47:47 -0700

    Fixed partial dependence plot function

--- R/pdpPlot.R
+++ R/pdpPlot.R
@@ -6,22 +6,27 @@
 #' @param data \code{data.frame} that was used to fit the \code{mod}
 #' @param covariates character vector of names of covariates to be used in QRF model
 #' @param plot_covars which covariates should be included in final plot
+#' @param covar_dict \code{data.frame} containing columns called ShortName and Name. ShortName corresponds to the covariates that went into the \code{mod}, while Name is a longer version for plotting purposes.
 #' @param quantiles which quantiles should be included on the partial dependence plots
 #' @param n_pts how many points to make predictions for
+#' @param same_y_scale should plots of different covariates be on the same y-axis scale? Default is \code{FALSE}.
 #'
 #' @author Kevin See
 #'
 #' @import tidyr magrittr dplyr ggplot2
+#' @importFrom plyr dlply ldply
 #' @export
 #' @return NULL
-#' @examples
+#' @examples pdpPlot(data = mod_data, mod = qrf_mod, covariates = hab_mets, covar_dict = hab_dict)
 
 pdpPlot = function(data = NULL,
                    mod = NULL,
                    covariates = NULL,
                    plot_covars = NULL,
+                   covar_dict = NULL,
                    quantiles = c(0.9),
-                   n_pts = 200) {
+                   n_pts = 200,
+                   same_y_scale = F) {
 
   if(is.null(plot_covars)) plot_covars = covariates
 
@@ -55,20 +60,24 @@ pdpPlot = function(data = NULL,
     filter(covar == Metric) %>%
     select(covar, value, matches('^quantile_')) %>%
     filter(covar %in% plot_covars) %>%
-    left_join(hab_dict %>%
+    left_join(covar_dict %>%
                 select(covar = ShortName, covar_label = Name)) %>%
     mutate(covar = factor(covar,
                           levels = relImportance(mod)$Metric),
            covar_label = factor(covar_label,
-                                levels = match(hab_dict$Name))) %>%
+                                levels = covar_dict$Name[match(relImportance(mod)$Metric, covar_dict$ShortName)])) %>%
     gather(quantile, pred, -covar_label, -covar, -value) %>%
     tbl_df()
 
   rug_df = select(data, WatershedName, one_of(hab_mets)) %>%
     gather(covar, value, -WatershedName) %>%
     filter(covar %in% plot_covars) %>%
-    left_join(hab_dict %>%
+    left_join(covar_dict %>%
                 select(covar = ShortName, covar_label = Name)) %>%
+    mutate(covar = factor(covar,
+                          levels = relImportance(mod)$Metric),
+           covar_label = factor(covar_label,
+                                levels = covar_dict$Name[match(relImportance(mod)$Metric, covar_dict$ShortName)])) %>%
     tbl_df()
 
   if(length(quantiles) == 1) {
@@ -76,10 +85,10 @@ pdpPlot = function(data = NULL,
              aes(x = value,
                  y = pred)) +
       geom_smooth(method = 'loess',
-                  se = F) +
+                  se = F,
+                  color = 'black') +
       geom_rug(data = rug_df, aes(x = value, y=NULL, color = WatershedName)) +
       scale_color_brewer(palette = 'Set1') +
-      facet_wrap(~ covar_label, scales = 'free') +
       theme_bw() +
       theme(legend.position = 'bottom') +
       labs(y = 'Prediction (fish / m)',
@@ -102,7 +111,6 @@ pdpPlot = function(data = NULL,
                    y = NULL),
                color = 'lightgray') +
       scale_color_brewer(palette = 'Set1') +
-      facet_wrap(~ covar_label, scales = 'free') +
       theme_bw() +
       theme(legend.position = 'bottom') +
       labs(y = 'Prediction (fish / m)',
@@ -111,10 +119,11 @@ pdpPlot = function(data = NULL,
            color = 'Quantile')
   }
 
+  if(!same_y_scale) pdp_p = pdp_p +
+    facet_wrap(~ covar_label, scales = 'free')
 
-
+  if(same_y_scale) pdp_p = pdp_p +
+    facet_wrap(~ covar_label, scales = 'free_x')
 
   return(pdp_p)
-
-
 }
